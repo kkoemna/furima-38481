609cbde8bf5edac6fc8704779218caae0f01e74f
kkoemna <115250897+kkoemna@users.noreply.github.com>
2022-12-21 23:15:26 +0900

    Active Hashの導入

--- Gemfile
+++ Gemfile
@@ -64,3 +64,4 @@ end
 gem 'devise'
 gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
+gem 'active_hash'
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :days_to_ship
 end
