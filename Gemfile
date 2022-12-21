290df12e1067cd0351771306c76cb67705371ba2
kkoemna <115250897+kkoemna@users.noreply.github.com>
2022-12-21 18:53:56 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -62,3 +62,5 @@ group :production do
 end
 
 gem 'devise'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
