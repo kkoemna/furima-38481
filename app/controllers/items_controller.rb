292b108634e94dfb065b03c189efeed35a4506bf
kkoemna <115250897+kkoemna@users.noreply.github.com>
2022-12-22 23:02:52 +0900

    出品した商品データの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+  
   def index
   end
 
@@ -8,11 +10,16 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,12 +1,12 @@
 class Condition < ActiveHash::Base
   self.data = [
-    { id: 0, name: '---' },
-    { id: 1, name: '新品・未使用' },
-    { id: 2, name: '未使用に近い' },
-    { id: 3, name: '目立った傷や汚れなし' },
-    { id: 4, name: 'やや傷や汚れあり' },
-    { id: 5, name: '傷や汚れあり' },
-    { id: 6, name: '全体的に状態が悪い' }
+    { id: 0, condition: '---' },
+    { id: 1, condition: '新品・未使用' },
+    { id: 2, condition: '未使用に近い' },
+    { id: 3, condition: '目立った傷や汚れなし' },
+    { id: 4, condition: 'やや傷や汚れあり' },
+    { id: 5, condition: '傷や汚れあり' },
+    { id: 6, condition: '全体的に状態が悪い' }
   ]
   include ActiveHash::Associations
   has_many :items
