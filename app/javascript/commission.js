fa2655f16f8d925788165605001d72d68b950ce0
kkoemna <115250897+kkoemna@users.noreply.github.com>
2022-12-23 16:50:01 +0900

    必要な要素を取得し入力のたびにイベント発火させる

--- app/javascript/commission.js
+++ app/javascript/commission.js
@@ -1,3 +1,6 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
+  })
 });
