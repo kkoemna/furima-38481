eaf19c809d75699b1653af63b6847aa1796b1b0b
kkoemna <115250897+kkoemna@users.noreply.github.com>
2022-12-26 17:00:43 +0900

    商品の詳細情報の表示

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,16 @@
 class Category < ActiveHash::Base
   self.data = [
-    { id: 0, category: '---' },
-    { id: 1, category: 'メンズ' },
-    { id: 2, category: 'レディース' },
-    { id: 3, category: 'ベビー・キッズ' },
-    { id: 4, category: 'インテリア・住まい・小物' },
-    { id: 5, category: '本・音楽・ゲーム' },
-    { id: 6, category: 'おもちゃ・ホビー・グッズ' },
-    { id: 7, category: '家電・スマホ・カメラ' },
-    { id: 8, category: 'スポーツ・レジャー' },
-    { id: 9, category: 'ハンドメイド' },
-    { id: 10, category: 'その他' }
+    { id: 0, name: '---' },
+    { id: 1, name: 'メンズ' },
+    { id: 2, name: 'レディース' },
+    { id: 3, name: 'ベビー・キッズ' },
+    { id: 4, name: 'インテリア・住まい・小物' },
+    { id: 5, name: '本・音楽・ゲーム' },
+    { id: 6, name: 'おもちゃ・ホビー・グッズ' },
+    { id: 7, name: '家電・スマホ・カメラ' },
+    { id: 8, name: 'スポーツ・レジャー' },
+    { id: 9, name: 'ハンドメイド' },
+    { id: 10, name: 'その他' }
   ]
   include ActiveHash::Associations
   has_many :items
