df2c124a31fb638c01a1e41a66c6d5b6e2e30905
kkoemna <115250897+kkoemna@users.noreply.github.com>
2023-01-11 19:54:49 +0900

    Formオブジェクトの作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,21 @@
+class OrderAddress
+
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :token, :post_code, :prefecture, :city, :address, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :token
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
+    validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, phone_number: phone_number, orer_id: order.id)
+  end
+end
