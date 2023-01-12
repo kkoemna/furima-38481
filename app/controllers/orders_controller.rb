c3622c56699f3cc325e8de100f566826cc0e554f
kkoemna <115250897+kkoemna@users.noreply.github.com>
2023-01-12 17:03:38 +0900

    エラーが表示されるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,12 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
-    @order_address.save
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
