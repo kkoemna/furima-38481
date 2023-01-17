b921ab157587b0cb529af08870a63c0438209110
kkoemna <115250897+kkoemna@users.noreply.github.com>
2023-01-17 16:51:45 +0900

    クレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :item_find
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :item_find,          only: [:index, :create]
+  before_action :redirect,           only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -9,6 +10,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -22,7 +24,23 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
   end
 
+  def redirect
+    if (@item.user_id == current_user.id) || (@item.order != nil)
+      redirect_to root_path
+    end
+  end
+
   def order_params
     params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
